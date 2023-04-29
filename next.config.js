445c4d4e1daba677d44102d9ce8b7b8841d1028c
John Raptis <johnsitpar@gmail.com>
2023-04-30 00:06:15 +0300

    turn app into a pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 const nextConfig = {
   reactStrictMode: true,
 };
@@ -14,16 +21,18 @@ module.exports = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
 
+// module.exports = withPWA({
+//   // next.js config
+// })
 
-
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/", // make '/' in v2 release
-  //       destination: "/welcome",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+// async redirects() {
+//   return [
+//     {
+//       source: "/", // make '/' in v2 release
+//       destination: "/welcome",
+//       permanent: true,
+//     },
+//   ];
+// },
