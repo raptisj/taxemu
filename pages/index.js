f3ef5748f5085087648e200effecb5f19e8969b0
John Raptis <johnsitpar@gmail.com>
2023-05-07 21:57:11 +0300

    add meta in index page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
+import { Meta } from "../components/meta";
 
 export default function Home() {
   return (
-    <Box minH="100vh" pt={0} pb={12} px="2rem" className="container">
-      <h1>Taxemu</h1>
-    </Box>
+    <>
+      <Meta />
+
+      <Box minH="100vh" pt={0} pb={12} px="2rem" className="container">
+        <h1>Taxemu</h1>
+      </Box>
+    </>
   );
 }
