1808bcc10b878c4abfff8887b84d3539bc117982
John Raptis <johnsitpar@gmail.com>
2023-05-07 19:01:17 +0300

    add gtag for analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "../styles/globals.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { hotjar } from 'react-hotjar'
+import { hotjar } from "react-hotjar";
+import { useRouter } from "next/router";
+import * as gtag from "../config/gtag";
+import Script from "next/script";
 
 const theme = extendTheme({
   styles: {
@@ -14,6 +17,18 @@ const theme = extendTheme({
 });
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   React.useEffect(() => {
     hotjar.initialize(
       process.env.NEXT_PUBLIC_HJID,
@@ -22,9 +37,29 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Script
+        id="g-1"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="g-2"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}');
+          `,
+        }}
+      />
+
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
